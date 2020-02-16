6391aeb298da853ea33f1816b943a98e40bf2900
tom37 <764575907@qq.com>
2020-02-17 01:26:16 +0800

    add makefile make中的路径搜索-下

---
+++ 3_Makefile/18-make中的路径搜索-下/code/src/func.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "func.h"
+
+void foo()
+{
+    //printf("void foo() : %s\n", "This file is from src ...");
+    printf("void foo() : %s\n", "This file is changed ...");
+}
